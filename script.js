5a76a23551bd1eac256531d57cfcb22f39900cfc
lolito-lindsey-official <lolitolindsey@gmail.com>
2024-01-06 17:33:45 -0600

    Added addtional comments to the button function

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 function toggleAccordion(button) {
+    // 'content refers to the next element is referring to the div element with an .accordion-answer class"
     var content = button.nextElementSibling;
-    // selects the icon elements from the HTML
+    // selects the icon elements from the HTML file
     let iconExpand = button.querySelector('.icon-expand');
     let iconCollapse = button.querySelector('.icon-collapse');
     
+    // if the content is clicked on by the user
     if (content.style.display === "block") {
+        // hides the accordion's answer, displays the expand icon, and hides the collapse icon
         content.style.display = "none";
-        // displays the expand icon and removes the collapse icon
         iconExpand.style.display = "block";
         iconCollapse.style.display = "none";
     } else {
+         // shows the accordion's answer,  displays the collapse icon, and hides the expand icon
         content.style.display = "block";
-        // displays the collapse icon and removes the expand icon
         iconExpand.style.display = "none";
         iconCollapse.style.display = "block";
     }
